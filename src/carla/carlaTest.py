e712cc1b47e4fe9278188538fae9bdaf5afb4dc5
raptor-i <55440860+raptor-i@users.noreply.github.com>
2023-05-06 16:11:34 +0300

    carla package added

--- src/carla/carlaTest.py
+++ src/carla/carlaTest.py
@@ -1,68 +1,33 @@
-from __future__ import print_function
+__author__ = "Erdi Örün, Mehmet Arif Bağcı, İlyas Çavdır"
+__copyright__ = "Copyright 2023, Trakya University"
+__version__ = "0.9.0"
+__status__ = "Development"
 
-
+import time
+import numpy as np
+import cv2
+import random
+import pygame
+import pyfiglet
 import glob
 import os
 import sys
 
 try:
-    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('./dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
 except IndexError:
     pass
 
-# ==============================================================================
-# -- imports -------------------------------------------------------------------
-# ==============================================================================
 import carla
-import time
-import numpy as np
-import cv2
-import random
-import pygame
 
-from pygame.locals import KMOD_CTRL
-from pygame.locals import KMOD_SHIFT
-from pygame.locals import K_0
-from pygame.locals import K_9
-from pygame.locals import K_BACKQUOTE
-from pygame.locals import K_BACKSPACE
-from pygame.locals import K_COMMA
-from pygame.locals import K_DOWN
-from pygame.locals import K_ESCAPE
-from pygame.locals import K_F1
-from pygame.locals import K_LEFT
-from pygame.locals import K_PERIOD
-from pygame.locals import K_RIGHT
-from pygame.locals import K_SLASH
-from pygame.locals import K_SPACE
-from pygame.locals import K_TAB
-from pygame.locals import K_UP
-from pygame.locals import K_a
-from pygame.locals import K_b
-from pygame.locals import K_c
-from pygame.locals import K_d
-from pygame.locals import K_f
-from pygame.locals import K_g
-from pygame.locals import K_h
-from pygame.locals import K_i
-from pygame.locals import K_l
-from pygame.locals import K_m
-from pygame.locals import K_n
-from pygame.locals import K_o
-from pygame.locals import K_p
-from pygame.locals import K_q
-from pygame.locals import K_r
-from pygame.locals import K_s
-from pygame.locals import K_t
-from pygame.locals import K_v
-from pygame.locals import K_w
-from pygame.locals import K_x
-from pygame.locals import K_z
-from pygame.locals import K_MINUS
-from pygame.locals import K_EQUALS
+
+ascii_banner = pyfiglet.figlet_format("Lane Detection")
+print(ascii_banner)
+print("version: " + __version__ + " \nstatus: " + __status__)
+
 
 IM_WIDTH = 640
 IM_HEIGHT = 480
